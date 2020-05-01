f7d92b681e789c579bcb6b4d2b5a2ab7f3878f3a
Ljunghster <mpljungh@gmail.com>
2020-04-30 22:20:48 -0700

    Testing to see what is appearing, so far not much

---
+++ planner.js
@@ -0,0 +1,34 @@
+var date;
+var eventDescription;
+var entryID;
+
+function initCalendarGrid() {
+
+}
+
+function getDate() {
+
+}
+
+function displayDate(entryID) {
+
+}
+
+function getDescription() {
+  console.log("getDescription reached");
+  eventDescription = document.getElementById('description').nodeValue;
+  
+  date = document.getElementById('dateSelect').value;
+  date = +date;
+  console.log(date);
+  console.log(eventDescription);
+    (function (){
+      document.getElementById(date).innerHtml = eventDescription;
+      console.log("inner func reached");
+    })();
+
+}
+
+function displayDescription(entryID) {
+
+}
